c7a4a92b8b9d4c043dd7f1ec9837b60a8950605c
r1gabriela <andradegabriela20@gmail.com>
2020-10-09 11:46:41 -0300

    commit 2

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import './App.css';
 import Todo from './TodoComponent';
 
-function App() {
-  return (
-    <div className="App">
+
+export default class App extends React.Component{
+
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return(
+      <div className="App">
       <header className="App-header"> 
           <Todo />
       </header>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
+
+
+
--- todolist/src/TodoComponent.js
+++ todolist/src/TodoComponent.js
@@ -1,48 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 
-export default class Todo extends React.Component {
-    constructor(props) {
-        super(props);
+
+export default class Todo extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = { items:[], text:''}
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.state = { items: [], text: '' }
     }
 
-    render() {
-        return (
+    render(){
+        return(
             <div>
-                <h1>Tarefas</h1>
+                <h3>Tarefas</h3>
                 <TodoList items={this.state.items} />
                 <form onSubmit={this.handleSubmit}>
-                    <label htmlFor='new-todo'>
-                        Adicionar tarefa
-                    </label>
-                    <br></br>
-                    <input
-                        id='new-todo'
-                        onChange={this.handleChange}
-                        value={this.state.text}
+                    <label htmlFor='new-todo'></label>
+                    <input 
+                    id='new-todo'
+                    onChange={this.handleChange}
+                    value={this.state.text}
                     />
-
                     <button>
-                        Adicionar #{this.state.items.length + 1}
+                        Adicionar#{this.state.items.length +1}
                     </button>
                 </form>
             </div>
         );
-    }
+    };
 
-    handleChange(e) {
+    handleChange(e){
         this.setState({ text: e.target.value })
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        if (this.state.text.length === 0) {
+    handleSubmit(e){
+        e.preventDefault()
+        if(this.state.text.length === 0){
             return;
-        }
+        };
 
         const newItem = {
             text: this.state.text,
@@ -50,27 +46,24 @@ export default class Todo extends React.Component {
         };
 
         this.setState(state => ({
-            items: state.items.concat(newItem),
+            items: this.state.items.concat(newItem),
             text: ' '
-
-        }))
+        }));
     }
 }
 
-class TodoList extends React.Component {
+class TodoList extends React.Component{
     render(){
         return(
             <ul>
-        {this.props.items.map(item => (
-            <li key={item.id}>{item.text}</li>
-        ))}
-    </ul>
+                {this.props.items.map(item=> (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
         )
     }
 }
 
-  
-       
 
 
 
@@ -82,3 +75,133 @@ class TodoList extends React.Component {
 
 
 
+
+
+
+
+
+
+
+
+
+//export default class Todo extends React.Component {
+
+//     state = {
+//         edit: false,
+//         id: null,
+//         mockData: [
+//             {
+//                 id: '1',
+//                 title: 'Buy Milk',
+//                 done: false,
+//                 date: new Date()
+//             },
+//             {
+//                 id: '2',
+//                 title: 'Study',
+//                 done: false,
+//                 date: new Date()
+//             },
+//             {
+//                 id: '3',
+//                 title: 'Run',
+//                 done: false,
+//                 date: new Date()
+//             }
+//         ]
+//     }
+
+
+//     render() {
+
+//         return (
+//             <div>
+//                 <form onSubmit={this.handleSubmit.bind(this)}>
+//                     <input
+//                         type='text'
+//                         name='item'
+//                         className='item'
+//                     />
+//                     <button className='btn-add-item'>
+//                         Add
+//                 </button>
+//                 </form>
+//                 <ul>
+//                     {this.state.mockData.map(item => (
+//                         <li key={item.id} className={item.done? 'done': 'hidden'}>
+//                             {item.title}<button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
+//                             <button onClick={this.handleEdit.bind(this, item.id, item.title)}>Edit</button>
+//                             <button onClick={this.handleComplete.bind(this)}>Complete</button>
+//                         </li>
+//                     ))}
+//                 </ul>
+//             </div>
+//         )
+//     }
+
+//     handleSubmit(e) {
+//         e.preventDefault();
+//         this.setState({ mockData: [...this.state.mockData, { id: Date.now() }, { title: e.target.item.value }, { done: false }, { date: new Date() }] });
+//         e.target.item.value = '';
+//     }
+
+//     handleDelete(e) {
+//         let id = arguments[0]
+//         this.setState({
+//             mockData: this.state.mockData.filter(item => {
+//                 if (item.id !== id) {
+//                     return item;
+//                 }
+//             }
+//             )
+//         })
+//     }
+
+//     renderEditForm(){
+//         if(this.state.edit){
+//             return(
+//                 <div>
+//                     <form onSubmit={this.handleUpdate.bind(this)}>
+//                     <input 
+//                     type='text'
+//                     name='updatedItem'
+//                     className='item'
+//                     defaultValue={this.state.title}
+//                     />
+//                     <button className='update-add-item'>
+//                         Update
+//                     </button>
+//                     </form>
+//                     {this.renderEditForm()}
+//                 </div>        
+//             )
+//         }
+//     }
+
+//     handleEdit(e){
+//         this.setState({ edit:true, id: arguments[0], title: arguments[1] });
+//     }
+
+//     handleUpdate(e){
+//         e.preventDefault();
+//         this.setState({ mockData: this.state.mockData.map(item =>{
+//             if(item.id === this.state.id){
+//                 item['title'] = e.target.updatedItem.value;
+//                 return item;
+//             }
+//             this.setState({ edit: false})
+//         }) })
+//     }
+
+//     handleComplete(){
+//         let id = arguments[0]
+//         this.setState( { mockData: this.state.mockData.map(item => {
+//             if(item.id === id){
+//                 item['done'] = true;
+//                 return item;
+//             }
+//             return item;
+//         })})
+//     }
+
+// }
